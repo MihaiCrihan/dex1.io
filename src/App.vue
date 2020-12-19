72e4200d6803783f65fe6ffe7fd2a47eaefc34d9
Mihai  Crihan <32567198+MihaiCrihan@users.noreply.github.com>
2020-12-19 03:44:00 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-app>
+    <v-app-bar
+      height="40"
+      app
+      elevate-on-scroll
+      dark
+    >
+      <div class="d-flex align-center heading-5">n
+        Сергей Лазарев
+      </div>
+
+      <v-spacer></v-spacer>
+      <div class="menus d-flex">
+        <div>
+          <a href="#songs">
+            Песни
+          </a>
+        </div>
+        <div>
+          <a href="#photo-galery">
+            Фото Галерея
+          </a>
+        </div>
+        <div>
+          <a href="#about" >
+            О певце
+          </a>
+        </div>
+        <div>
+          <a href="#eurovision">
+            Проекты
+          </a>
+        </div>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
+<style>
+  ::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px grey;
+    border-radius: 10px;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #4f4f4f;
+    border-radius: 10px;
+  }
+
+  .menus >>> a {
+    text-decoration: none;
+    color: #FFFFFF;
+    display: inline-block;
+  }
+  .menus div {
+    margin: 0 8px;
+  }
+
+  .v-application a {
+    color: #ffffff !important;
+  }
+  a:link {
+    text-decoration: none;
+  }
+
+  a:visited {
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+  a:active {
+    text-decoration: none;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,186 @@
+<template>
+  <div>
+    <v-row class="text-center">
+      <v-img style="height: calc(100vh - 40px);" :src="require('../assets/img/sergej-lazarev.jpg')"></v-img>
+
+    </v-row>
+    <div class="bloks">
+      <v-row id="songs" class="pa-4">
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <iframe src="https://www.youtube.com/embed/WqSwmevIXHg" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen>
+          </iframe>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <iframe src="https://www.youtube.com/embed/eNzlxEZ_JG4" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen>
+          </iframe>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <iframe src="https://www.youtube.com/embed/e94dst20C9Y" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen>
+          </iframe>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <iframe src="https://www.youtube.com/embed/gHgxi57Um0w" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen>
+          </iframe>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <iframe src="https://www.youtube.com/embed/y6t5HmDFDww" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen>
+          </iframe>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <iframe src="https://www.youtube.com/embed/4m25OSlKfWs" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen>
+          </iframe>
+        </v-col>
+      </v-row>
+      <v-row id="photo-galery" class="pa-4">
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <v-img src="../assets/img/2.jpg"/>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <v-img src="../assets/img/4.jpg"/>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <v-img src="../assets/img/5.jpg"/>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <v-img src="../assets/img/6.jpg"/>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <v-img src="../assets/img/3.jpeg"/>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <v-img src="../assets/img/6.jpg"/>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <v-img src="../assets/img/7.jpg"/>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <v-img src="../assets/img/8.jpg"/>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+          <v-img src="../assets/img/9.jpg"/>
+        </v-col>
+
+      </v-row>
+      <v-row style="margin: 0 auto; overflow-y: scroll ; overflow-x: hidden !important;"  id="about" class=" mt-3 mb-3">
+        <h1 style="margin: 0 auto" class="pa-2">О певце</h1>
+        <v-row class="justify-center">
+          <v-col cols="12" xs="12" sm="5" md="5" lg="5" xl="5" class="mt-4" style="font-size: 18px">
+            Родился 1 апреля 1983 года в Москве. С 4 лет занимался спортивной гимнастикой.  Лазарев прекратил посещение
+            спортивных секций и продолжил занятия в различных детских музыкальных ансамблях. С 9 до 11 лет Сергей
+            Лазарев
+            пел в ансамбле имени Владимира Локтева вместе с братом Павлом. В это же время играл в театре Бориса
+            Покровского. В 1995 году стал одним из участников детского ансамбля «Непоседы». В том же году снялся в телевизионном
+            юмористическом
+            журнале «Ералаш».<br><br>
+
+            <h3 class="pl-3">Сольная карьера</h3><br>
+            1 декабря 2005 года вышел дебютный альбом Лазарева, получивший название Don’t Be Fake. В его состав вошли 12
+            композиций С начала 2006 года в эфире российских радиостанций появилась первая русскоязычная композиция
+            Лазарева — баллада «Даже если ты уйдёшь». <br><br>
+
+            <h3 class="pl-3">Личная жизнь</h3>
+
+            С Лерой Кудрявцевой на вечеринке программы «Пусть говорят» 10 сентября 2010 года
+            С 2008 по 2012 год Лазарев встречался с телеведущей Лерой Кудрявцевой. Эти отношения
+            подвергались активному обсуждению в прессе. После расставания Сергей и Лера сохранили дружеские
+            отношения.<br>
+
+            23 декабря 2016 года певец признался, что у него в 2014 году родился сын Никита.<br>
+
+            29 сентября 2019 года в программе «Секрет на миллион» певец признался, что год назад у него родилась дочь
+            Анна.
+          </v-col>
+            <v-col cols="12" xs="12" sm="5" md="5" lg="5" xl="5" class="ml-4 justify-center text-center ">
+              <v-img width="450" src="../assets/img/a4.png"/>
+            </v-col>
+        </v-row>
+      </v-row>
+      <div id="eurovision" style="overflow-y: scroll; overflow-x: hidden">
+        <h2 style="margin: 0 auto;" class="text-center mt-8 mb-4">Участие в проектах</h2>
+        <v-row >
+          <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+            <v-img src="../assets/img/masca.jpg"/>
+          </v-col>
+
+          <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+            <v-img src="../assets/img/logica.jpg"/>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+            <v-img src="../assets/img/teatr.jpg"/>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+            <v-img src="../assets/img/vmeste.jpg"/>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+            <v-img src="../assets/img/eurov.jpg"/>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+            <v-img src="../assets/img/7.jpg"/>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+            <v-img src="../assets/img/eurov2.jpg"/>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+            <v-img src="../assets/img/9.jpg"/>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="6" lg="4" xl="4">
+            <v-img src="../assets/img/improv.jpg"/>
+          </v-col>
+
+
+        </v-row>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({}),
+}
+</script>
+<style>
+  .bloks {
+    width: 94%;
+    align-content: center;
+    justify-content: center;
+    margin: 0 auto;
+  }
+
+  .bloks .row {
+    height: 100vh;
+  }
+
+  .bloks iframe {
+    width: 100%;
+    height: 100%;
+  }
+
+  #photo-galery {
+    height: 100vh;
+    max-height: 100vh;
+    overflow-y: scroll;
+  }
+
+  #photo-galery .v-img {
+    width: auto;
+    height: 444px !important;
+    max-height: 444px !important;
+  }
+</style>
